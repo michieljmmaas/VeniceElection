d81c129fb544332ea550f9be6c40a3114b72fed1
moxio\mmaas <michiel.maas@moxio.com>
2021-05-27 13:28:22 +0200

    Initial Commit

---
+++ base.ts
@@ -0,0 +1,7 @@
+let people = []
+
+for(let i = 0; i < 100; i++) {
+  people.push(Math.floor(Math.random() * 11));
+}
+
+console.log(people)
---
+++ main.js
@@ -0,0 +1,7 @@
+people = []
+
+for(i = 0; i < 100; i++) {
+  people.push(Math.floor(Math.random() * 11));
+}
+
+console.log(people)
