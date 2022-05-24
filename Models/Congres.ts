528d3b6385ba8d1167f717a9526c1cc3b186783d
moxio\mmaas <michiel.maas@moxio.com>
2022-05-24 14:34:55 +0200

    NEw methods

--- Models/Congres.ts
+++ Models/Congres.ts
@@ -9,8 +9,25 @@ export class Congres {
     constructor(selectedPeople: Person[], notSelectedPeople: Person[],  Families: Family[]) {
         this.selectedPeople = selectedPeople;
         this.notSelectedPeople = notSelectedPeople;
-        this.Families = Families;
-      }
+        this.Families = Families;  
+   }
+
+   selectPerson(new_person: Person) {
+     this.selectedPeople.push(new_person);
+     this.notSelectedPeople = this.notSelectedPeople.filter(person => person.$Id !== new_person.$Id);
+}
+
+getElligiblePeople(): Person[] {
+  let selected_families = this.getSelectedFamilyIds();
+  return this.notSelectedPeople.filter(person => !selected_families.includes(person.$Family.$Id))
+}
+
+
+
+
+   getSelectedFamilyIds() {
+    return this.selectedPeople.map(person => person.$Family.$Id);
+   }
 
       printData() {
         console.log("Congres Data: ")
